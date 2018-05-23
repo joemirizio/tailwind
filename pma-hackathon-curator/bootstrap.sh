24d1551fbc4ee456e04dfbb6f66ea45914d14d5b
Joe Mirizio <joe@joemirizio.com>
2018-05-23 18:40:28 -0400

    Fix bootstrap.sh error

--- pma-hackathon-curator/bootstrap.sh
+++ pma-hackathon-curator/bootstrap.sh
@@ -3,6 +3,6 @@
 python manage.py migrate
 python manage.py loadmodels ${API_URI}
 python manage.py loaddata $(ls curator/fixtures/*.json)
-pipenv run python manage.py loadfacts curator/fixtures/manual/artwork_facts.json
+python manage.py loadfacts curator/fixtures/manual/artwork_facts.json
 
 python manage.py runserver 0.0.0.0:8000
