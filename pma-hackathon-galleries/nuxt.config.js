ac8d93d2a49805bca69545e06aeb90c7f4dd1dc6
Joe Mirizio <joe@joemirizio.com>
2018-05-25 02:53:34 -0400

    Replace GA code with a nuxt module

--- pma-hackathon-galleries/nuxt.config.js
+++ pma-hackathon-galleries/nuxt.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   css: [
     '@assets/css/global.css'
-  ]
+  ],
+  modules: [
+    ['@nuxtjs/google-analytics']
+  ],
+  'google-analytics': {
+    id: 'UA-119873309-1'
+  }
 }
