919076eaf4e8b6174d5b8c6b40d7462980c5faae
Joe Mirizio <joe@joemirizio.com>
2018-05-25 01:28:01 -0400

    Add Google Analytics

--- pma-hackathon-galleries/layouts/default.vue
+++ pma-hackathon-galleries/layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
-  <nuxt/>
+    <nuxt>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-119873309-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-119873309-1');
+    </script>
+    </nuxt>
 </template>
 
 <script>
