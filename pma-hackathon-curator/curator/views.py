36443dc2c8c7b8d84ec131028d8a44e8890fa881
Joe Mirizio <joe@joemirizio.com>
2018-05-25 01:13:25 -0400

    Add persona reactions to recommendation scores

--- pma-hackathon-curator/curator/views.py
+++ pma-hackathon-curator/curator/views.py
@@ -54,10 +54,11 @@ def get_gallery_recommendation(request, gallery_id):
   reason = ''
   if visitor_reactions.filter(reaction_type__value__gt=0).exists():
     try:
-      recommendations = recommendations_visitor(visitor_id, gallery_id)
+      recommendations = get_visitor_recommendations(visitor_id, gallery_id)
       artwork = recommendations[0]['artwork']
       reason = recommendations[0]['description']
-    except:
+    except Exception as e:
+      print(e)
       raise Http404("No more personal recommendations")
   else:
     # For gallery, no visitor reactions, so choose based on persona reactions
@@ -67,7 +68,7 @@ def get_gallery_recommendation(request, gallery_id):
     if not filtered_reactions:
       raise Http404("No more persona recommendations")
     artwork = Artwork.objects.get(pk=filtered_reactions[0])
-    reason = 'Highly rated amongst {}s. React positively to this piece to see more like this one!'.format(persona.name)
+    reason = 'Highly rated amongst {}s. React positively to this piece to get personalized recommendations that are similar to this one!'.format(persona.name)
 
   # Add a "no response" default reaction
   react(visitor_id, NO_RESPONSE, artwork.id)
@@ -121,6 +122,8 @@ def add_visitor(request):
   except ObjectDoesNotExist:
     return _dict_serialize({ 'error': 'Object does not exist' })
 
+def get_persona(visitor_id):
+  return Visitor.objects.get(pk=visitor_id).persona
 
 def _json_serialize(objects):
   data = serializers.serialize('json', objects)
@@ -135,31 +138,54 @@ def index(request):
   #return render(request, 'index.html')
 
 
-def recommendations_visitor(visitor_id, gallery_id):
+def get_visitor_recommendations(visitor_id, gallery_id):
+  persona = get_persona(visitor_id)
+  visitor = str(visitor_id)
+  gallery = str(gallery_id)
+
   with connection.cursor() as cursor:
     cursor.execute('''
-      SELECT 
+    SELECT 
         B.artwork_id, 
         group_concat(replace(B.key, '|', '.'), '|') AS reason, 
-        group_concat(replace(B.value, '|', '.'), '|') AS rationale, 
-        SUM(B.weight + REACTIONS.score) AS weight
+        group_concat(replace(B.value, '|', '.'), '|') AS rationale,
+        IFNULL(PERSONA_REACTIONS.score, 0) as persona_liked,
+        SUM(B.weight + USER_REACTIONS.score + (IFNULL(PERSONA_REACTIONS.score, 0) * 2)) AS weight
       FROM (
         SELECT * FROM curator_artworkattribute ORDER BY weight DESC
       ) AS B
       JOIN (
+	      -- Get all attributes and calculate a score for all 
+		    -- positive user reactions in all galleries
         SELECT SUM(RT.value * A.weight) AS score, A.key, A.value
         FROM curator_artworkattribute A
         JOIN curator_reaction R ON A.artwork_id = R.artwork_id
         JOIN curator_reactiontype RT ON R.reaction_type_id = RT.id
         JOIN curator_artwork ART ON R.artwork_id = ART.id
-        WHERE R.visitor_id = %s AND RT.value > 0
+        WHERE R.visitor_id = %s AND RT.value > 0 AND A.value != 'Artist/maker unknown'
         GROUP BY A.key, A.value
         ORDER BY score DESC
-      ) AS REACTIONS ON 
-          REACTIONS.key = B.key AND
-          REACTIONS.value = B.value
+      ) AS USER_REACTIONS ON
+          USER_REACTIONS.key = B.key AND
+          USER_REACTIONS.value = B.value
+      LEFT JOIN (
+        -- Get artwork and score of persona reactions in gallery
+        SELECT ART.id as artwork_id, SUM(RT.value) as score
+        FROM curator_reaction R
+        JOIN curator_reactiontype RT ON R.reaction_type_id = RT.id
+        JOIN curator_artwork ART ON R.artwork_id = ART.id
+        JOIN curator_visitor V ON R.visitor_id = V.id
+        JOIN curator_persona P ON V.persona_id = P.id
+        WHERE P.id = %s AND ART.gallery_id = %s
+        GROUP BY ART.id
+      ) AS PERSONA_REACTIONS ON 
+          PERSONA_REACTIONS.artwork_id = B.artwork_id
+      JOIN curator_artwork as ARTWORK ON B.artwork_id = ARTWORK.id
       WHERE
-        B.artwork_id NOT in (
+        -- In the given gallery
+        ARTWORK.gallery_id = %s AND
+        -- Not something that was already recommended to the user
+        B.artwork_id NOT IN (
           SELECT R.artwork_id
           FROM curator_reaction R 
           JOIN curator_reactiontype RT ON R.reaction_type_id = RT.id
@@ -168,11 +194,11 @@ def recommendations_visitor(visitor_id, gallery_id):
         )
       GROUP BY B.artwork_id
       ORDER BY weight DESC;
-    ''', [str(visitor_id), str(visitor_id), str(gallery_id)])
+    ''', [visitor, persona.id, gallery, gallery, visitor, gallery])
     rows = cursor.fetchall()
 
-  rows = [row_append(row, recommendation_descriptions(row)) for row in rows]
-  recommendations = [dict(weight=row[3], description=row[4], artwork=Artwork.objects.get(pk=row[0])) for row in rows]
+  rows = [row_append(row, recommendation_descriptions(row, persona.name)) for row in rows]
+  recommendations = [dict(weight=row[4], description=row[5], artwork=Artwork.objects.get(pk=row[0])) for row in rows]
   context = {'recommendations': recommendations}
   return recommendations
 
@@ -196,9 +222,10 @@ def react(visitor_id, reaction_type_id, artwork_id):
     reaction = Reaction(visitor=visitor, reaction_type=reaction, artwork=artwork).save()
   return reaction
 
-def recommendation_descriptions(recommendation):
+def recommendation_descriptions(recommendation, persona_name):
   reasons = recommendation[1].split('|')
   rationales = recommendation[2].split('|')
+  persona_num_liked = int(recommendation[3])
   messages = []
 
   for i, reason in enumerate(reasons):
@@ -207,23 +234,31 @@ def recommendation_descriptions(recommendation):
 
     if reason == 'GalleryShort':
       messages.append('located in this gallery')
-    if reason == 'Wing':
+    elif reason == 'Wing':
       if not 'GalleryShort' in reasons:
         messages.append('located in this wing')
-    if reason == 'Classification':
-      # TODO Add appropriate pluralization
+    elif reason == 'DateBegin':
+      messages.append('made in {}'.format(rationale))
+    elif reason == 'DateEnd':
+      messages.append('completed in {}'.format(rationale))
+    elif reason == 'Classification':
       messages.append('{}'.format(rationaleLowerCase))
-    if reason == 'Style':
+    elif reason == 'Style':
       messages.append('in the {} style'.format(rationaleLowerCase))
-    if reason == 'Movement':
+    elif reason == 'Movement':
       messages.append('in the {} movement'.format(rationale))
-    if reason == 'SocialTags':
+    elif reason == 'SocialTags':
       messages.append('have to do with {}'.format(rationale))
     
   # Add 'and'
   message_length = len(messages)
   if message_length > 1:
     messages[message_length - 1] = 'and ' + messages[message_length - 1]
+  
+  # Add sentence for persona likes
+  persona_message = ' {} other {}{} liked this too.'.format(
+    persona_num_liked, persona_name, 's' if persona_num_liked > 1 else '') if persona_num_liked > 0 else ''
 
   message = ', '.join(messages) if (message_length > 2) else ' '.join(messages)
-  return 'You like pieces that are ' + message;
+
+  return 'You like pieces that are ' + message + '.' + persona_message
